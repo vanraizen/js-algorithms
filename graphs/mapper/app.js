131b9cebc59e7250a76eb9693c255af4087bdcbf
Fadi Chalfoun <fadichalfoun@gmail.com>
2015-12-07 15:44:01 -0800

    * Updated mapper application to correctly perform BFS and mark map nodes for each step

--- graphs/mapper/app.js
+++ graphs/mapper/app.js
@@ -1,3 +1,61 @@
 angular
     .module('mapper', [])
-    .controller('MapperController', function() {});
+    .controller('MapperController', function($scope) {
+        $scope.startDfs = function () {
+            $scope.$broadcast('startDfs');
+        };
+        $scope.startBfs = function () {
+            $scope.$broadcast('startBfs');
+        };
+        $scope.logMapState = function () {
+            $scope.$broadcast('logMapState');
+        };
+    })
+    .service('BFS', function($log, $rootScope, $interval) {
+
+        function bfs (startingNode) {
+
+            var visited = [],
+                queue = [],
+                runIntervalId;
+
+            queue.push(startingNode);
+            $rootScope.$broadcast('markNodes', { node: startingNode, color: 'yellow' });
+
+            runIntervalId = $interval(visit, 1000);
+
+            function visit() {
+                var visitNode = queue.splice(0, 1)[0],
+                    adjacentNodes,
+                    nodesToQueue;
+                $log.log('Visiting -> ', visitNode);
+                $rootScope.$broadcast('markNodes', { node: visitNode, color: 'green' });
+                visited.push(visitNode);
+                $log.log('Visited is now -> ', visited);
+                adjacentNodes = visitNode.getConnections();
+                $log.debug('Adjacent Nodes -> ', adjacentNodes);
+                nodesToQueue = [];
+                adjacentNodes.forEach(function(adjacentNode) {
+                    if (visited.indexOf(adjacentNode) === -1 && queue.indexOf(adjacentNode) === -1) {
+                        console.log('Adding to unvisited -> ', adjacentNode);
+                        nodesToQueue.push(adjacentNode);
+                    }
+                });
+                Array.prototype.push.apply(queue, nodesToQueue);
+                $log.log('Queue is now -> ', queue);
+                if(queue.length) {
+                    $rootScope.$broadcast('markNodes', { nodes: nodesToQueue, color: 'yellow' });
+                } else {
+                    $log.log('BFS complete...');
+                    $interval.cancel(runIntervalId);
+                }
+            }
+        }
+
+        return {
+            go: function (startingNode, nodes) {
+                $log.log('starting BFS with node', startingNode.id);
+                bfs(startingNode, nodes);
+            }
+        }
+    });
--- graphs/mapper/mapper.js
+++ graphs/mapper/mapper.js
@@ -6,24 +6,66 @@
  */
 angular
     .module('mapper')
-    .directive('map', ['$log', '$http',
-        function ($log, $http) {
+    .directive('map', ['$log', 'BFS',
+        function ($log, BFS) {
 
             function Map() {
+
                 var map = this;
                 this.nextId = 1;
                 this.nodes = [];
+                this.edges = [];
+
                 this.addNode = function (x,y) {
                     this.nodes.push(new Node(x,y));
                     this.nextId++;
                 };
+
                 this.getNodes = function () { return this.nodes; };
+
+                this.addEdge = function (node1Id, node2Id) {
+                    var node1 = getNode(node1Id),
+                        node2 = getNode(node2Id);
+                    node1.addConnectionTo(node2);
+                    node2.addConnectionTo(node1);
+                    this.edges.push({x1: node1.x, y1: node1.y, x2: node2.x, y2: node2.y});
+                };
+
+                this.bfs = function (nodeId) { BFS.go(getNode(nodeId)) };
+
+                this.getEdges = function () { return this.edges };
+
+                this.resetColors = function () {
+                    this.nodes.forEach(function(node) {
+                        node.resetColor();
+                    });
+                };
+
                 function Node(x,y) {
+
                     this.id = map.nextId;
                     this.x = x;
                     this.y = y;
-                    this.color = 'purple';
+                    this.radius = 10;
                     this.connections = [];
+
+                    this.addConnectionTo = function (otherNode) {
+                        this.connections.push(otherNode);
+                    };
+                    this.getConnections = function () {
+                        return this.connections;
+                    };
+
+                    this.resetColor = function () {
+                        this.color = 'purple';
+                    };
+                    this.resetColor();
+                }
+
+                function getNode(nodeId) {
+                    var nodeMatch = false;
+                    map.nodes.forEach(function(node) { if(node.id == nodeId) nodeMatch = node;  });
+                    return nodeMatch;
                 }
             }
 
@@ -35,18 +77,61 @@ angular
                 replace: true,
                 templateUrl: 'map.html',
                 link: function ($scope, element, attrs) {
-                    var map = new Map();
-                    console.log(d3);
+
+                    var map = new Map(),
+                        nodeClick1Id,
+                        startDfs,
+                        startBfs;
+
+                    $scope.edges = map.getEdges();
                     $scope.nodes = map.getNodes();
+
                     $scope.handleClick = function ($event) {
-                        console.log($event);
+                        var nodeClickId,
+                            nodeClick2Id;
+                        //console.log($event);
                         if($event.toElement.tagName === 'svg') {
                             map.addNode($event.x, $event.y);
                         } else {
-                            var nodeId = $event.target.innerHTML;
-                            console.log('link start', nodeId);
+                            nodeClickId = $event.target.attributes.getNamedItem('node-id').nodeValue;
+                            if(startBfs) {
+                                map.resetColors();
+                                map.bfs(nodeClickId);
+                                startBfs = false;
+                            } else if (startDfs) {
+
+                            } else {
+                                if(nodeClick1Id) {
+                                    nodeClick2Id = nodeClickId;
+                                    console.log('link end', nodeClick2Id);
+                                    map.addEdge(nodeClick1Id, nodeClick2Id);
+                                    nodeClick1Id = nodeClick2Id = null;
+                                } else {
+                                    nodeClick1Id = nodeClickId;
+                                    console.log('link start', nodeClick1Id);
+                                }
+                            }
                         }
                     };
+
+                    $scope.$on('startDfs', function () {
+                        startDfs = true;
+                    });
+
+                    $scope.$on('startBfs', function () {
+                        startBfs = true;
+                    });
+
+                    $scope.$on('logMapState', function () {
+                        $log.log('nodes', map.getNodes(), 'edges', map.getEdges());
+                    });
+
+                    $scope.$on('markNodes', function (event, args) {
+                        var nodes = args.nodes || [args.node];
+                        nodes.forEach(function(node) {
+                            node.color = args.color;
+                        })
+                    });
                 }
             };
         }]);
