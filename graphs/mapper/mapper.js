82b84ec10be5547cf2bfcee2e53704f5fd021481
Fadi Chalfoun <fadichalfoun@gmail.com>
2015-12-07 19:49:01 -0800

    Added a guide for indicating the path of a new edge to make improve experience adding a new node

--- graphs/mapper/mapper.js
+++ graphs/mapper/mapper.js
@@ -6,8 +6,11 @@
  */
 angular
     .module('mapper')
-    .directive('map', ['$log', 'BFS',
-        function ($log, BFS) {
+    .directive('map', ['$log', 'BFS', '$document',
+        function ($log, BFS, $document) {
+
+            var startDfs,
+                startBfs;
 
             function Map() {
 
@@ -23,15 +26,14 @@ angular
 
                 this.getNodes = function () { return this.nodes; };
 
-                this.addEdge = function (node1Id, node2Id) {
-                    var node1 = getNode(node1Id),
-                        node2 = getNode(node2Id);
-                    node1.addConnectionTo(node2);
-                    node2.addConnectionTo(node1);
-                    this.edges.push({x1: node1.x, y1: node1.y, x2: node2.x, y2: node2.y});
+                this.addEdge = function (node1, node2) {
+                    if (node1.addConnectionTo(node2)) {
+                        node2.addConnectionTo(node1);
+                        this.edges.push({x1: node1.x, y1: node1.y, x2: node2.x, y2: node2.y});
+                    }
                 };
 
-                this.bfs = function (nodeId) { BFS.go(getNode(nodeId)) };
+                this.bfs = function (nodeId) { BFS.go(this.getNode(nodeId)) };
 
                 this.getEdges = function () { return this.edges };
 
@@ -50,7 +52,12 @@ angular
                     this.connections = [];
 
                     this.addConnectionTo = function (otherNode) {
-                        this.connections.push(otherNode);
+                        if(this.connections.indexOf(otherNode) === -1) {
+                            this.connections.push(otherNode);
+                            return true;
+                        }
+                        $log.debug('connection from ', this, 'to ', otherNode, 'already exists');
+                        return false;
                     };
                     this.getConnections = function () {
                         return this.connections;
@@ -62,11 +69,94 @@ angular
                     this.resetColor();
                 }
 
-                function getNode(nodeId) {
+                this.getNode = function (nodeId) {
                     var nodeMatch = false;
                     map.nodes.forEach(function(node) { if(node.id == nodeId) nodeMatch = node;  });
                     return nodeMatch;
                 }
+            }
+
+            function NewEdgeGuide(map) {
+
+                var guide = this,
+                    map = map;
+
+                this.reset = function () {
+                    $document.find('body').css('cursor', 'default');
+                    this.show = false;
+                    this.color = 'red';
+                };
+                this.reset();
+
+                this.registerMouseLocation = function (x, y) {
+                    if(this.show) {
+                        this.x2 = x;
+                        this.y2 = y;
+                        if(this.x1 > this.x2) {
+                            this.x2 = this.x2 - 1;
+                        } else {
+                            this.x2 = this.x2 + 1;
+                        }
+                        if(this.y1 > this.y2) {
+                            this.y2 = this.y2 + 1;
+                        } else {
+                            this.y2 = this.y2 - 1;
+                        }
+                    }
+                };
+
+                this.nodeClicked = function (nodeId) {
+                    var node = map.getNode(nodeId);
+                    $log.log('node clicked', nodeId, node);
+                    if (this.show) {
+                        end(node);
+                    } else {
+                        start(node);
+                    }
+                };
+
+                this.mouseEntered = function (node) {
+                    if (this.show && this.startingNode.id != node.id) {
+                        this.color = 'green';
+                    }
+                };
+
+                function start (node) {
+                    $document.find('body').css('cursor', 'crosshair');
+                    guide.show = true;
+                    guide.startingNode = node;
+                    guide.x1 = node.x;
+                    guide.y1 = node.y;
+                    guide.x2 = node.x;
+                    guide.y2 = node.y;
+                }
+
+                function end (node) {
+                    console.log(guide.startingNode, node);
+                    map.addEdge(guide.startingNode, node);
+                    guide.reset();
+                }
+            }
+
+            function registerListeners($scope) {
+                $scope.$on('startDfs', function () {
+                    startDfs = true;
+                });
+
+                $scope.$on('startBfs', function () {
+                    startBfs = true;
+                });
+
+                $scope.$on('logMapState', function () {
+                    $log.log('nodes', map.getNodes(), 'edges', map.getEdges());
+                });
+
+                $scope.$on('markNodes', function (event, args) {
+                    var nodes = args.nodes || [args.node];
+                    nodes.forEach(function(node) {
+                        node.color = args.color;
+                    })
+                });
             }
 
             'use strict';
@@ -79,20 +169,18 @@ angular
                 link: function ($scope, element, attrs) {
 
                     var map = new Map(),
-                        nodeClick1Id,
-                        startDfs,
-                        startBfs;
+                        guide = new NewEdgeGuide(map);
 
                     $scope.edges = map.getEdges();
                     $scope.nodes = map.getNodes();
+                    $scope.newEdgeGuide = guide;
 
                     $scope.handleClick = function ($event) {
-                        var nodeClickId,
-                            nodeClick2Id;
+                        var nodeClickId;
                         //console.log($event);
-                        if($event.toElement.tagName === 'svg') {
+                        if ($event.toElement.tagName === 'svg') {
                             map.addNode($event.x, $event.y);
-                        } else {
+                        } else if ($event.toElement.tagName === 'circle') {
                             nodeClickId = $event.target.attributes.getNamedItem('node-id').nodeValue;
                             if(startBfs) {
                                 map.resetColors();
@@ -101,37 +189,25 @@ angular
                             } else if (startDfs) {
 
                             } else {
-                                if(nodeClick1Id) {
-                                    nodeClick2Id = nodeClickId;
-                                    console.log('link end', nodeClick2Id);
-                                    map.addEdge(nodeClick1Id, nodeClick2Id);
-                                    nodeClick1Id = nodeClick2Id = null;
-                                } else {
-                                    nodeClick1Id = nodeClickId;
-                                    console.log('link start', nodeClick1Id);
-                                }
+                                guide.nodeClicked(nodeClickId);
+                            }
+                        } else {
+                            if(guide.show) {
+                                guide.reset();
                             }
                         }
                     };
 
-                    $scope.$on('startDfs', function () {
-                        startDfs = true;
-                    });
+                    registerListeners($scope);
 
-                    $scope.$on('startBfs', function () {
-                        startBfs = true;
-                    });
+                    $scope.registerMouseLocation = function($event) {
+                        $scope.newEdgeGuide.registerMouseLocation($event.x, $event.y);
+                    };
 
-                    $scope.$on('logMapState', function () {
-                        $log.log('nodes', map.getNodes(), 'edges', map.getEdges());
-                    });
-
-                    $scope.$on('markNodes', function (event, args) {
-                        var nodes = args.nodes || [args.node];
-                        nodes.forEach(function(node) {
-                            node.color = args.color;
-                        })
-                    });
+                    $scope.nodeMouseEntered = function (node) {
+                        node.color = 'blue';
+                        guide.mouseEntered(node);
+                    };
                 }
             };
         }]);
