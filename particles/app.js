c34a77a29285b3308e8931681f1e95a91c37da5f
Fadi Chalfoun <fadichalfoun@gmail.com>
2015-12-09 23:39:04 -0800

    Optimized movement to make it look better

--- particles/app.js
+++ particles/app.js
@@ -1,6 +1,6 @@
 angular
     .module('mapper', [])
-    .service('MagicParticles', function($interval, $document, $window, $log) {
+    .service('MagicParticles', function($interval, $document, $window) {
         var clientWidth = $window.innerWidth,
             clientHeight = $window.innerHeight,
             particles = [],
@@ -12,59 +12,70 @@ angular
 
             this.x = iniX;
             this.y = iniY;
-            this.ctx = ctx;
 
             this.randomize = function () {
                 this.color = colors[Math.floor(Math.random()*4)];
                 this.radius = radius * Math.random() + 1;
-                this.yAcceleration = Math.random()*100 + 1;
-                this.xAcceleration = Math.random()*25 - Math.random()*25 + 1;
+                //this.radius = radius;
+                //this.yAcceleration = this.xAcceleration = 0;
+                this.yAcceleration = 1;
+                this.xAcceleration = Math.random()*5 - Math.random()*5;
+                this.draws = 0;
+                this.glimmers = this.xAcceleration > 0;
             };
 
             this.randomize();
 
-            this.draw = function (step) {
+            this.draw = function (step, wave) {
 
-                var ctx = this.ctx,
-                    stepSine = Math.sin(step),
-                    intensityOffset = (step%10)/10,
+                ++this.draws;
+
+                //wave is some decimal number that pulsates from 0-1
+
+                var intensityOffset = wave,
                     x = this.x + this.xAcceleration,
-                    y = this.y - this.yAcceleration,
+                    y = this.y - this.yAcceleration  - this.draws/4,
                     i,
                     particle = this;
 
-                this.x = x;
-                if (y < 0) {
+                if (y < 0 || x > clientWidth || x < 0) {
                     y = clientHeight;
-                    this.x = Math.random() * clientWidth;
+                    x = (Math.random()*25)+clientWidth/2;
                     this.randomize();
                 }
+                this.x = x;
                 this.y = y;
 
                 function composeStarShape(radius) {
 
                     var boundaryDistance = (particle.radius - radius) / particle.radius,
-                        intensity = Math.floor((255 * boundaryDistance) * intensityOffset),
+                        intensity = particle.glimmers ? Math.floor((255 * boundaryDistance) * intensityOffset) : 0,
                         lineColor = genColor();
 
+                    radius = (radius*(wave+particle.xAcceleration/4)) + (radius/4);
+
+                    //top left quadrant
                     ctx.beginPath();
                     ctx.moveTo(x-radius, y);
                     ctx.strokeStyle = lineColor;
                     ctx.quadraticCurveTo(x, y, x, y-radius);
                     ctx.stroke();
 
+                    //top right quadrant
                     ctx.beginPath();
                     ctx.moveTo(x+radius, y);
                     ctx.strokeStyle = lineColor;
                     ctx.quadraticCurveTo(x, y, x, y-radius);
                     ctx.stroke();
 
+                    //bottom right quadrant
                     ctx.beginPath();
                     ctx.moveTo(x+radius, y);
                     ctx.strokeStyle = lineColor;
                     ctx.quadraticCurveTo(x, y, x, y+radius);
                     ctx.stroke();
 
+                    //bottom left quadrant
                     ctx.beginPath();
                     ctx.moveTo(x-radius, y);
                     ctx.strokeStyle = lineColor;
@@ -96,26 +107,28 @@ angular
                 canvas.attr('width', clientWidth + 'px');
 
                 for (i = 0; i < numberOfParticles; i++) {
-                    particles.push(new MagicParticle(Math.random()*clientWidth, 0, 20, ctx));
+                    particles.push(new MagicParticle(clientWidth/2, Math.random()*clientHeight, 20, ctx));
                 }
 
                 return this;
 
             },
             draw: function() {
-                var step = 0;
+                var step = 0,
+                    wave;
                 $interval(function() {
                     ++step;
+                    wave = (Math.sin(step/10)+1)/2;
                     ctx.clearRect(0, 0, clientWidth, clientHeight);
                     particles.forEach(function(particle) {
-                        particle.draw(step);
+                        particle.draw(step, wave);
                     })
-                }, 50);
+                }, 35);
             }
         }
     })
     .controller('ParticlesController', function($scope, MagicParticles) {
         MagicParticles
-            .generate(200)
+            .generate(100)
             .draw();
     });
