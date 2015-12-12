5f9c2e4292e6449d9617077d62dddec840a0a234
Fadi Chalfoun <fadichalfoun@gmail.com>
2015-12-11 22:49:44 -0800

    Added different color modes for the concentric circle visualization

--- concentricCircles/app.js
+++ concentricCircles/app.js
@@ -4,26 +4,104 @@ angular
         var clientWidth = $window.innerWidth,
             clientHeight = $window.innerHeight,
             circlesAdded = 0;
-        $log.log($window);
-        function FollowingCircle(size) {
-            var circle = this;
+        //$log.log($window);
+
+        function Circle(id, size) {
+            var circle = this,
+                colorFunction;
+            this.id = id;
             this.x = clientWidth / 2;
             this.y = clientHeight / 2;
             this.size = size;
-            $interval(function () {
-                circle.r = Math.floor(Math.random() * 255);
-                circle.g = Math.floor(Math.random() * 255);
-                circle.b = Math.floor(Math.random() * 255);
-            }, 500);
+            this.a = 1;
+
+            this.draw = function (step, mode) {
+                if(mode === 'random') {
+                    colorFunction = randomColors;
+                } else if(mode === 'hypno') {
+                    colorFunction = hypnoticColors;
+                } else if(mode === 'insanity') {
+                    colorFunction = hypnoticColors;
+                } else if(mode === 'out') {
+                    colorFunction = out;
+                }  else if(mode === 'in') {
+                    colorFunction = inColors;
+                }
+                console.log(mode);
+                colorFunction(circle, step);
+            };
         }
+
+        function randomColors (circle) {
+            circle.r = Math.floor(Math.random() * 255);
+            circle.g = Math.floor(Math.random() * 255);
+            circle.b = Math.floor(Math.random() * 255);
+        }
+
+        function hypnoticColors(circle, step) {
+            if (circle.id % 2 == 0) {
+                if (step % 2 == 0) {
+                    black(circle);
+                } else {
+                    white(circle);
+                }
+            } else {
+                if (step % 2 == 0) {
+                    white(circle);
+                } else {
+                    black(circle);
+                }
+            }
+        }
+
+        function out (circle, step) {
+            if (step%circlesAdded == circle.id) {
+                blue(circle);
+            } else {
+                black(circle);
+            }
+        }
+
+        function inColors (circle, step) {
+            if ((circlesAdded - step%circlesAdded) == circle.id) {
+                blue(circle);
+            } else {
+                black(circle);
+            }
+        }
+
+        function blue(circle) {
+            circle.r = 0;
+            circle.g = 0;
+            circle.b = 255;
+        }
+
+        function white(circle) {
+            circle.r = 255;
+            circle.g = 255;
+            circle.b = 255;
+        }
+
+        function black(circle) {
+            circle.r = 0;
+            circle.g = 0;
+            circle.b = 0;
+        }
+
         return {
             generate: function(circlesToAdd) {
                 ++circlesAdded;
-                var size = Math.round(clientWidth - ((circlesAdded/circlesToAdd) * clientWidth));
+                var size = Math.round((clientWidth - ((circlesAdded/circlesToAdd) * clientWidth)) / 2);
+                //console.log(size);
                 if(size) {
-                    return new FollowingCircle(size);
+                    return new Circle(circlesAdded, size);
                 }
             }
         }
     })
-    .controller('ConcentricCirclesController', function($scope) {});
+    .controller('ConcentricCirclesController', function($scope, $location) {
+        $scope.location = $location;
+        $scope.$watch('location.search()', function() {
+            $scope.$broadcast('mode', $location.search().mode);
+        });
+    });
--- concentricCircles/cc.js
+++ concentricCircles/cc.js
@@ -6,27 +6,63 @@
  */
 angular
     .module('mapper')
-    .directive('circles', ['$log', 'CircleGenerator',
-        function ($log, CircleGenerator) {
+    .directive('circles', ['$log', 'CircleGenerator', '$interval',
+        function ($log, CircleGenerator, $interval) {
 
             'use strict';
             return {
                 // used as an element only (e.g., <circles></circles>)
                 restrict: 'E',
+                //scope: {
+                //  mode: '='
+                //},
                 // replace the entire element with the contents of viewer
                 replace: true,
                 templateUrl: 'map.html',
                 link: function ($scope, element, attrs) {
 
                     var circles = 50,
-                        i;
+                        i,
+                        speed,
+                        step,
+                        mode = 'random',
+                        intervalId;
+
                     $scope.circles = [];
                     for(i = 0; i < circles; i++) {
                         var newCircle = CircleGenerator.generate(circles);
                         if (newCircle) {
-                            $log.log(newCircle);
+                            //$log.debug(newCircle);
                             $scope.circles.push(newCircle);
                         }
+                    }
+
+                    $scope.$on('mode', function (event, newMode) {
+                        $log.debug('new color mode: ', newMode);
+                        mode = newMode;
+                        run();
+                    });
+
+                    run();
+
+                    function run() {
+                        step = 0;
+                        if(mode === 'random') {
+                            speed = 100;
+                        } else if(mode === 'hypno') {
+                            speed = 300;
+                        } else if(mode === 'insanity') {
+                            speed = 10;
+                        } else if(mode === 'out' || mode === 'in') {
+                            speed = 10;
+                        }
+                        if (intervalId) {
+                            $interval.cancel(intervalId);
+                        }
+                        intervalId = $interval(function () {
+                            ++step;
+                            $scope.circles.forEach(function (circle) { circle.draw(step, mode); })
+                        }, speed);
                     }
                 }
             };
